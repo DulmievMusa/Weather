7f351c645bfb4c7c18a270a6bdfe984d80eea631
Musa Dulmiev <musadulmiev@gmail.com>
2023-02-15 18:24:30 +0300

    Flask Version 1.2

--- app.py
+++ app.py
@@ -10,11 +10,8 @@ app = Flask(__name__)
 def index(query=''):
     if request.method == 'POST':
         return redirect('/' + request.form['query'])
-    print(request.method)
     if request.method == 'POST':
         query = request.form['query']
-        print(123)
-        print(query)
     with open('design.html', 'r', encoding='utf-8') as html_stream:
         html = html_stream.read()
         weather = ws.get_weather(query)
