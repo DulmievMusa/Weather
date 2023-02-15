6acc535facb0a1d250194b54b50e6705e598efca
Musa Dulmiev <musadulmiev@gmail.com>
2023-02-15 17:18:57 +0300

    Flask Version 0.1

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+import ws
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    with open('design.html', 'r', encoding='utf-8') as html_stream:
+        html = html_stream.read()
+    weather = ws.get_weather()
+
+
+
+if __name__ == '__main__':
+    app.run(port=8000, host='127.0.0.1', debug=True)
